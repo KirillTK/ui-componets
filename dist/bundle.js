54669da5220f3ba109f5d52c637479b543201311
k2.tkachyov <k2.tkachyov@itransition.com>
2020-03-13 14:09:30 +0300

    sadasdsadasdsadasdasda

---
+++ dist/bundle.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
+
+var React = _interopDefault(require('react'));
+
+var getText = function () { return 'Test'; };
+
+var Button = function () { return React.createElement("button", { className: "button-new" }, getText()); };
+
+exports.Button = Button;
